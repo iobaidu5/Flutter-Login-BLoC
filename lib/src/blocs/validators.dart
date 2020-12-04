eec49fabbba82e31911b3ee62ed7c483c0feecfd
iobaidu5 <i.obaidu5@gmail.com>
2020-12-03 21:02:47 -0800

    bloc

---
+++ lib/src/blocs/validators.dart
@@ -0,0 +1,21 @@
+import 'dart:async';
+
+class Validator {
+  final validateEmail =
+      StreamTransformer<String, String>.fromHandlers(handleData: (email, sink) {
+    if (email.contains('@')) {
+      sink.add(email);
+    } else {
+      sink.addError('Add valid email');
+    }
+  });
+
+  final validatePassword = StreamTransformer<String, String>.fromHandlers(
+      handleData: (password, sink) {
+    if (password.length > 3) {
+      sink.add(password);
+    } else {
+      sink.addError("Password Must be atleast 4 characters");
+    }
+  });
+}
